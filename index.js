132c73b9022936c2bf14b65dd2f9d07123d0cbfa
AbdulRazzak157 <abdulrazzak@skygoalnext.com>
2024-08-13 16:40:26 +0530

    added console 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log('Hello Everyone, testing repo project');
 console.log('tournament is completed');
 console.log('help everyone , working');
+console.log('"hello everyone whats going on ');
